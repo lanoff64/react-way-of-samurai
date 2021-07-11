9d0de88bd04e352e94199dfbddbce812999cdc2c
lanoff64 <lanov64@gmail.com>
2021-07-12 01:40:49 +0600

    new way of samurai

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import Header from "./components/Header/Header";
+import Profile from "./components/Profile/Profile"
+import {Route} from "react-router-dom";
+import Music from "./components/Music/Music";
+import News from "./components/News/News";
+import Settings from "./components/Settings/Settings";
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import NavBarContainer from "./components/NavBar/NavBarContainer";
+import UsersContainer from "./components/Users/UsersContainer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = (props) => {
+    return (
+
+        <div className='app-wrapper'>
+
+            <Header/>
+            <NavBarContainer/>
+
+            <div className='app-wrapper-content'>
+                <Route path='/dialogs'
+                       render={() => <DialogsContainer/>}/>
+                <Route path='/profile'
+                       render={() => <Profile/>}/>
+                <Route path='/users'
+                       render={() => <UsersContainer/> }/>
+                <Route path='/music' component={Music}/>
+                <Route path='/news' component={News}/>
+                <Route path='/settings' component={Settings}/>
+
+            </div>
+
+
+        </div>
+
+    );
 }
 
 export default App;
