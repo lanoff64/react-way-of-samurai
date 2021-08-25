17d9942ab5e4fc8a53fee0014152bce9723aca9b
lanoff64 <lanov64@gmail.com>
2021-08-25 17:12:51 +0600

    add Redux dev tools EXTENSION

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import dialogsPageReducer from "./dialogsPageReducer";
 import profilePageReducer from "./profilePageReducer";
 import navBarReducer from "./navBarReducer";
@@ -16,9 +16,9 @@ let reducers = combineReducers({
     app:appReducer
 });
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
 
-
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
 
 
 window.store = store;
