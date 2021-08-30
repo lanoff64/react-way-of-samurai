ead3d6361e1eb4bb0c8dedd32ce381a5ad4adcb8
lanoff64 <lanov64@gmail.com>
2021-08-31 03:17:04 +0600

    94 React Lazy, Suspense

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import './App.css';
-import React from "react";
+import React, {Suspense} from "react";
 import {Route, withRouter} from "react-router-dom";
+import NavBarContainer from "./components/NavBar/NavBarContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 import Music from "./components/Music/Music";
 import News from "./components/News/News";
 import Settings from "./components/Settings/Settings";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import NavBarContainer from "./components/NavBar/NavBarContainer";
-import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
-import HeaderContainer from "./components/Header/HeaderContainer";
-import LoginContainer from "./components/commons/LoginContainer";
 import {connect} from "react-redux";
 import {compose} from "redux";
 import {initializeAppThunk} from "./redux/appReducer";
 import Preloader from "./components/commons/Preloader/Preloader";
+//import DialogsContainer from "./components/Dialogs/DialogsContainer";
+// import UsersContainer from "./components/Users/UsersContainer";
+//import LoginContainer from "./components/commons/LoginContainer";
+const LoginContainer = React.lazy( () => import("./components/commons/LoginContainer"));
+const DialogsContainer = React.lazy(() => import("./components/Dialogs/DialogsContainer"));
+const UsersContainer = React.lazy( () => import("./components/Users/UsersContainer"));
 
 
 class App extends React.Component {
@@ -25,6 +28,7 @@ class App extends React.Component {
             return <Preloader/>
         }
         return (
+            <Suspense fallback={<Preloader/>}>
             <div className='app-wrapper'>
                 <HeaderContainer/>
                 <NavBarContainer/>
@@ -41,6 +45,7 @@ class App extends React.Component {
                     <Route path='/login' component={LoginContainer}/>
                 </div>
             </div>
+            </Suspense>
 
         );
     }
