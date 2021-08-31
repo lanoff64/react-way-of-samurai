6e2a7458954024c2b325d24b93c67c12408acbea
lanoff64 <lanov64@gmail.com>
2021-09-01 04:07:37 +0600

    add hook for selecet photo

--- src/components/Profile/Description/Description.jsx
+++ src/components/Profile/Description/Description.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import classes from './Disc.module.css';
 import Preloader from "../../commons/Preloader/Preloader";
 import UserPhoto from "./../../../assets/images/no-user-26.jpg"
 
 
 const Description = ({profile, ...props}) => {
+    let [editMode, setEditMode] = useState(false);
+
+    const activeEditMode = () => {
+        setEditMode(true);
+    }
+
+
 
     if (!profile) {
         return <Preloader/>
@@ -13,19 +20,30 @@ const Description = ({profile, ...props}) => {
     const onMainPhotoSelected = (e) => {
         if (e.target.files.length) {
             props.savePhotoThunk(e.target.files[0]);
+            setEditMode(false);
+
         }
     }
 
     const imgSrc = profile.photos.large || UserPhoto;
+
     return (
-        <div className={classes.avaAndDesc}>
-            <div className={classes.ava}>
+        <div className={classes.avaAndDesc} onClick={()=>setEditMode(false)}>
+            <div className={classes.ava} onDoubleClick={activeEditMode}>
                 {props.isDownload ? <div className={classes.loading}>...loading</div> : <img src={imgSrc} alt="avatar"/>}
+
+                {!editMode && null}
+                {editMode &&
+                    <span>
+                {props.isOwner ? <input disabled={props.isDownload} type="file" onChange={onMainPhotoSelected}/> : null}
+                    </span>}
             </div>
-            {props.isOwner ? <input disabled={props.isDownload} type="file" onChange={onMainPhotoSelected}/> : null}
+
             <div className={classes.about}>
                 <div className={classes.userDecsName}>{profile.fullName}</div>
                 <div className={classes.divDescLabel}>ID: {profile.userId}</div>
+
+
                 <div className={classes.divDesc}>
                     <div><span className={classes.divDescLabel}>О себе:</span> {profile.aboutMe}</div>
                     <div><span
