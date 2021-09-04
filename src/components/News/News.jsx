439f462a24aaa0d48d7d1574a4e1650c8af4e1cf
lanoff64 <lanov64@gmail.com>
2021-09-05 03:41:52 +0600

    simple feed news

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,15 +1,49 @@
 import React from 'react';
-import classes from './News.module.css'
 
 
 const News = (props) => {
+    const lenta = <iframe title={'news'} width="800" height="350" className="news"
+                           src="http://feeds.feedburner.com/lenta/MpsS"/>;
+
+    const zakonKz = <iframe title={'news'} width="800" height="350" className="news"
+                            src="http://feeds.feedburner.com/zakon/vWos"/>;
+
+    const mailKz = <iframe title={'news'} width="800" height="350" className="news"
+                           src="http://feeds.feedburner.com/mail/YeGC"/>;
+
+    const kurs = <iframe title={'news'} width="800" height="350" className="news"
+                           src="http://feeds.feedburner.com/com/cMiY"/>;
+
+    const tenge = <iframe title={'news'} width="800" height="350" className="news"
+                           src="http://feeds.feedburner.com/kzt"/>;
+
+
     return (
-        <div className={classes.news}>
+        <div>
+            <div>
+                {lenta}
+            </div>
 
-            News
 
+            <div>
+                {zakonKz}
+            </div>
+
+            <div>
+                {mailKz}
+            </div>
+
+            <div>
+                {kurs}
+            </div>
+
+            <div>
+                {tenge}
+            </div>
 
         </div>
+
+
     );
 
 }
