06994f522d65284e96910293904d153a9048c173
lanoff64 <lanov64@gmail.com>
2021-09-09 02:32:37 +0600

    links form for tests no validation

--- src/components/Profile/ProfileLinks/LinksForm.jsx
+++ src/components/Profile/ProfileLinks/LinksForm.jsx
@@ -1,76 +1,72 @@
 import React from "react";
 import classes from "../Description/Disc.module.css";
 import {ErrorMessage, Field, Form, Formik} from "formik";
-import * as yup from "yup";
 
 
 
 
 const LinksFormFormik = ({initialValues, outEditMode, ...props}) => {
 
-    // let badURLS = Object.keys(initialValues.contacts);
-    const validationSchema = yup.object().shape({
-        // badURLS: yup.string().required('поле обязательно').oneOf(badURLS)
-    })
+
     return (
         <div className={classes.formiklinks}>
             <Formik
                 initialValues={initialValues}
-                validationSchema={validationSchema}
                 onSubmit={(values, {setStatus}) => {
+
                     props.saveContacts(values, setStatus);//thunk
                 }}
             >
                 {formik =>
                     (
-                        <Form onSubmit={formik.handleSubmit}>
+                        <Form onSubmit={formik.handleSubmit}  >
                                     <div>
-                                        <label htmlFor="contacts.facebook">facebook</label>
-                                        <Field name="contacts.facebook" type="text" placeholder={'facebook'}/>
-                                        <ErrorMessage name="contacts.facebook"/>
+                                        <label htmlFor='contacts.facebook'>facebook</label>
+                                        <Field name='contacts.facebook'  type="text" placeholder={'facebook'}/>
+                                        <ErrorMessage name='contacts.facebook'/>
                                     </div>
 
                                     <div>
-                                        <label htmlFor="contacts.website">website</label>
-                                        <Field name="contacts.website" type="text" placeholder={'website'}/>
-                                        <ErrorMessage name="contacts.website"/>
+                                        <label htmlFor='contacts.website'>website</label>
+                                        <Field name='contacts.website' type="text" placeholder={'website'}/>
+                                        <ErrorMessage name='contacts.website'/>
                                     </div>
 
-                                    <div>
-                                        <label htmlFor="contacts.vk">vk</label>
-                                        <Field name="contacts.vk" type="text" placeholder={'vk'}/>
-                                        <ErrorMessage name='contacts.vk'/>
-                                    </div>
+                                    {/*<div>*/}
+                                    {/*    <label htmlFor={vk}>vk</label>*/}
+                                    {/*    <Field name={vk} type="text" placeholder={'vk'}/>*/}
+                                    {/*    <ErrorMessage name={vk}/>*/}
+                                    {/*</div>*/}
 
-                                    <div>
-                                        <label htmlFor="contacts.twitter">twitter</label>
-                                        <Field name="contacts.twitter" type="text" placeholder={'twitter'}/>
-                                        <ErrorMessage name='contacts.twitter'/>
-                                    </div>
+                                    {/*<div>*/}
+                                    {/*    <label htmlFor={twitter}>twitter</label>*/}
+                                    {/*    <Field name={twitter} type="text" placeholder={'twitter'}/>*/}
+                                    {/*    <ErrorMessage name={twitter}/>*/}
+                                    {/*</div>*/}
 
-                                    <div>
-                                        <label htmlFor="contacts.instagram">instagram</label>
-                                        <Field name="contacts.instagram" type="text" placeholder={'instagram'}/>
-                                        <ErrorMessage name='contacts.instagram'/>
-                                    </div>
+                                    {/*<div>*/}
+                                    {/*    <label htmlFor={instagram}>instagram</label>*/}
+                                    {/*    <Field name={instagram} type="text" placeholder={'instagram'}/>*/}
+                                    {/*    <ErrorMessage name={instagram}/>*/}
+                                    {/*</div>*/}
 
-                                    <div>
-                                        <label htmlFor="contacts.youtube">youtube</label>
-                                        <Field name="contacts.youtube" type="text" placeholder={'youtube'}/>
-                                        <ErrorMessage name='contacts.youtube'/>
-                                    </div>
+                                    {/*<div>*/}
+                                    {/*    <label htmlFor={youtube}>youtube</label>*/}
+                                    {/*    <Field name={youtube} type="text" placeholder={'youtube'}/>*/}
+                                    {/*    <ErrorMessage name={youtube}/>*/}
+                                    {/*</div>*/}
 
-                                    <div>
-                                        <label htmlFor="contacts.github">github</label>
-                                        <Field name="contacts.github" type="text" placeholder={'github'}/>
-                                        <ErrorMessage name='contacts.github'/>
-                                    </div>
+                                    {/*<div>*/}
+                                    {/*    <label htmlFor={github}>github</label>*/}
+                                    {/*    <Field name={github} type="text" placeholder={'github'}/>*/}
+                                    {/*    <ErrorMessage name={github}/>*/}
+                                    {/*</div>*/}
 
-                                    <div>
-                                        <label htmlFor="contacts.mainLink">mainLink</label>
-                                        <Field name="contacts.mainLink" type="text" placeholder={'mainLink'}/>
-                                        <ErrorMessage name='contacts.mainLink'/>
-                                    </div>
+                                    {/*<div>*/}
+                                    {/*    <label htmlFor={mainLink}>mainLink</label>*/}
+                                    {/*    <Field name={mainLink} type="text" placeholder={'mainLink'}/>*/}
+                                    {/*    <ErrorMessage name={mainLink}/>*/}
+                                    {/*</div>*/}
 
                                 <button type="submit">Save</button>
                                 <button onClick={outEditMode}>Cancel</button>
